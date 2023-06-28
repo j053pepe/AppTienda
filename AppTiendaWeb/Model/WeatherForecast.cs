3286e3e4972786f7c71a59e5310cd891917b11ff
Jose Guadalupe Rodriguez Flores <jrodriguez@redcompanies.com.mx>
2023-06-27 20:32:56 -0600

    Cambios pendientes

---
+++ AppTiendaWeb/Model/WeatherForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.AppTiendaWeb.Model
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; internal set; }
+        public int TemperatureC { get; internal set; }
+        public string Summary { get; internal set; }
+    }
+}
