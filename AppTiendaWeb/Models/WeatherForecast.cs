836ae2b127d5d1aa0991bf7488a9b853af47e7ff
Jose Guadalupe Rodriguez Flores <jrodriguez@redcompanies.com.mx>
2023-07-01 20:16:51 -0600

    Nuevos Proyectos para librerias que se usaran

--- AppTiendaWeb/Models/WeatherForecast.cs
+++ AppTiendaWeb/Models/WeatherForecast.cs
@@ -1,9 +1,12 @@
-﻿namespace Presentation.AppTiendaWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.AppTiendaWeb.Models
 {
     public class WeatherForecast
     {
         public DateTime Date { get; internal set; }
         public int TemperatureC { get; internal set; }
+        [MaxLength(200)]
         public string Summary { get; internal set; }
     }
 }
